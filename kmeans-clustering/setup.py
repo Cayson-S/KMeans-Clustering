1cc63408087878742514f2c9589d32e0944406c1
Cayson <crs9g@mtmail.mtsu.edu>
2023-03-27 10:57:20 -0500

    created the k-means repository.

---
+++ kmeans-clustering/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project was created for my AI class. In it, I coded the K-Means',
+    author='Cayson',
+    license='',
+)
